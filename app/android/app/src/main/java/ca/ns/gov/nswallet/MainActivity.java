a9c3eebdf67e1f0892eba1c25f675148e9193411
Srinivasan, Dinesh <dinesh.srinivasan@novascotia.ca>
2023-01-05 20:10:56 -0400

    NW-8 - CI/CD to distribute Android and IOS releases to App Center [skip ci]

--- app/android/app/src/main/java/ca/ns/gov/nswallet/MainActivity.java
+++ app/android/app/src/main/java/ca/ns/gov/nswallet/MainActivity.java
@@ -1,17 +1,13 @@
-package ca.bc.gov.BCWallet;
+package ca.ns.gov.nswallet;
+
 import android.os.Bundle;
 import android.system.ErrnoException;
 import android.system.Os;
 import java.io.File;
-
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -23,7 +19,13 @@ public class MainActivity extends ReactActivity {
       e.printStackTrace();
     }
   }
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
   protected String getMainComponentName() {
-    return "BCWallet";
+    return "NSWallet";
   }
 }
